4611f5d60a06cc75c6e397788ae4f6ad8e96f344
Luke Bond <luke.n.bond@gmail.com>
2020-01-22 10:20:15 +0000

    reordered preferred tile layouts

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -70,10 +70,10 @@ altkey = "Mod1"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    awful.layout.suit.tile.top,
     awful.layout.suit.tile,
-    awful.layout.suit.tile.bottom,
     awful.layout.suit.tile.left,
+    awful.layout.suit.tile.top,
+    awful.layout.suit.tile.bottom,
     awful.layout.suit.max,
     awful.layout.suit.max.fullscreen,
     awful.layout.suit.floating,
